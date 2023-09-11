149783761005caa467e3306d77efdd9e486d0683
saaxbutcool <saad.elm.77@gmail.com>
2023-09-11 17:42:25 +0100

    first react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import logo from "./Color Fill 2 copy.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>stay connected</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
